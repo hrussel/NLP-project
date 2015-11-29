eae6efc0fd6ecd633dee32fd8c10159eb41a96b6
baris demirdelen <barisdemirdelen@gmail.com>
2015-11-29 17:27:37 +0100

    added pps and oargs into string spans

--- src/RecipeReader.java
+++ src/RecipeReader.java
@@ -28,58 +28,57 @@ public class RecipeReader {
             Action currentAction = null;
             while (sc.hasNextLine()) {
                 String s = sc.nextLine();
-                if(s.contains("SENTID: ")) {
+                if (s.contains("SENTID: ")) {
                     String line = sc.nextLine();
                     //System.out.println(line);
-                    line = line.substring(s.indexOf("SENT: ")+6);
+                    line = line.substring(s.indexOf("SENT: ") + 6);
                     //String sentence = sc.nextLine().substring()
 
                 }
 
-                if(s.contains("PRED: ")) {
-                    String predicateString = s.substring(s.indexOf("PRED: ")+6);
-                    StringSpan predicate = new StringSpan(predicateString,0,0);
+                if (s.contains("PRED: ")) {
+                    String predicateString = s.substring(s.indexOf("PRED: ") + 6);
+                    StringSpan predicate = new StringSpan(predicateString, 0, 0);
                     currentAction = new Action();
                     currentAction.setPredicate(predicate);
                     recipe.getActions().add(currentAction);
-                    System.out.println("T"+tId+ " predicate "+ predicate.getStart()+" "+predicate.getEnd()+" "+predicate.getWord());
-                    tId++;
                 }
 
-                if(s.contains("DOBJ: ")) {
-                    String argumentStringFull = s.substring(s.indexOf("DOBJ: ")+6);
+                if (s.contains("DOBJ: ")) {
+                    String argumentStringFull = s.substring(s.indexOf("DOBJ: ") + 6);
                     String[] argumentStrings = argumentStringFull.split(",");
                     Argument argument = new Argument();
-                    for(String argumentString : argumentStrings) {
+                    for (String argumentString : argumentStrings) {
                         argumentString = argumentString.trim();
-                        StringSpan argumentSpan = new StringSpan(argumentString,0,0);
+                        StringSpan argumentSpan = new StringSpan(argumentString, 0, 0);
                         argument.getWords().add(argumentSpan);
                         //argument.setSemanticType();
                         argument.setSyntacticType(SyntacticType.DOBJ);
-                        System.out.println("T"+tId+ " arg_object "+ argumentSpan.getStart()+" "+argumentSpan.getEnd()+" "+argumentSpan.getWord());
-                        tId++;
                     }
                     currentAction.getArguments().add(argument);
                 }
 
-                if(s.contains("PARG: ")) {
-                    String argumentStringFull = s.substring(s.indexOf("PARG: ")+6);
-                    String[] argumentStrings = argumentStringFull.split(",");
+                if (s.contains("PARG: ")) {
+                    String argumentString = s.substring(s.indexOf("PARG: ") + 6);
+                    String prep = sc.nextLine();
+                    prep = prep.substring(prep.indexOf("PREP: ") + 6);
                     Argument argument = new Argument();
-                    for(String argumentString : argumentStrings) {
-                        argumentString = argumentString.trim();
-                        StringSpan argumentSpan = new StringSpan(argumentString,0,0);
-                        argument.getWords().add(argumentSpan);
-                        argument.setSemanticType(SemanticType.OTHER);
-                        argument.setSyntacticType(SyntacticType.PP);
-                        System.out.println("T"+tId+ " arg_"+argument.getSemanticType()+ " "+ argumentSpan.getStart()+" "+argumentSpan.getEnd()+" "+argumentSpan.getWord());
-                        tId++;
-                    }
+                    StringSpan argumentSpan = new StringSpan(prep + " " + argumentString, 0, 0);
+                    argument.getWords().add(argumentSpan);
+                    argument.setSemanticType(SemanticType.OTHER);
+                    argument.setSyntacticType(SyntacticType.PP);
+
                     currentAction.getArguments().add(argument);
                 }
 
+                if (s.contains("OARG: ")) {
+                    String oargString = s.substring(s.indexOf("OARG: ") + 6);
+                    StringSpan predicate =  currentAction.getPredicate();
+                    predicate.setWord(predicate.getWord() + " " + oargString);
+                }
 
-               // System.out.println(s);
+
+                // System.out.println(s);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -88,7 +87,29 @@ public class RecipeReader {
                 sc.close();
             }
         }
-
+        printRecipe(recipe);
         return recipe;
+    }
+
+
+    public void printRecipe(Recipe recipe) {
+        tId=1;
+        for(Action action : recipe.getActions()) {
+            StringSpan predicate = action.getPredicate();
+            System.out.println("T" + tId + " predicate " + predicate.getStart() + " " + predicate.getEnd() + " " + predicate.getWord());
+            tId++;
+
+            for(Argument argument : action.getArguments()) {
+                for(StringSpan argumentSpan : argument.getWords()) {
+                    SemanticType semanticType = argument.getSemanticType();
+                    String semanticTypeString = "OBJECT";
+                    if(semanticType!=null) {
+                        semanticTypeString = semanticType.toString();
+                    }
+                    System.out.println("T" + tId + " arg_" + semanticTypeString + " " + argumentSpan.getStart() + " " + argumentSpan.getEnd() + " " + argumentSpan.getWord());
+                    tId++;
+                }
+            }
+        }
     }
 }
